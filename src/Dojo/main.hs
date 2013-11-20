dd019efacd244d2ba4f70bfe2a761f541bfaaa03
vagrant <vagrant@haskell>
2013-11-20 23:25:48 +0000

    Final do dojo

--- src/Dojo/main.hs
+++ src/Dojo/main.hs
@@ -5,8 +5,13 @@ data Book = Book1 | Book2 | Book3 deriving (Eq, Show)
 data Money a = USD a deriving (Eq, Show)
 
 price :: Fractional b => [Book] -> Money b
+
 price [a,b] 
 	| a /= b = USD 15.2
 	| otherwise = USD 16.0
+
 price books = USD (8.0 * fromIntegral(length(books)))
-	
+
+count books book = (length . filter (\x -> x == book)) books
+
+--alldifferent books = all (\x -> count(x,books) == 1) books
--- test/Dojo/MainSpec.hs
+++ test/Dojo/MainSpec.hs
@@ -8,12 +8,21 @@ import Dojo.Main
 
 spec :: Spec
 spec = do
-  describe "Calculate Bowling score" $ do
+  describe "count" $ do
+    it "returns 1 when there is only one occurence of the element in array" $ do
+      count ["one", "two", "many"] "one" `shouldBe` 1
+    it "returns 2 when there are two occurences of the element in array" $ do
+      count ["one", "two", "many", "one"] "one" `shouldBe` 2
+  describe "Calculate Bowling score (wat?)" $ do
     it "calculates the price for 1 book" $ do
       price [Book1] `shouldBe` USD 8.0
     it "calculates the price 2 of the same books" $ do
       price [Book1, Book1] `shouldBe` USD 16.0
     it "calculates the price 2 different books" $ do
       price [Book1, Book2] `shouldBe` USD 15.2
+    it "calculates the price 2 different books again" $ do
+      price [Book2, Book3] `shouldBe` USD 15.2
     it "calculates the price 2 different books" $ do
       price [Book1, Book3] `shouldBe` USD 15.2
+    it "calculates the price 3 different books" $ do
+      price [Book1, Book2, Book3] `shouldBe` USD 21.6
